b56b961066f7efde6c65098bda73d39d83e7e3a3
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-11-02 08:00:24 +0100

    login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
 import Authentication from '../controllers/auth/signup/signup.auth.controller';
-
+import LoginAuth from '../controllers/auth/login/login.auth.controller';
 
 const router = Router();
 
 router.post('/patient/signup', Authentication.PatientSignup);
 router.post('/doctor/signup', Authentication.DoctorSignup);
 router.post('/nurse/signup', Authentication.NurseSignup);
+
+router.post('/login', LoginAuth.Login);
 
 export default router;
