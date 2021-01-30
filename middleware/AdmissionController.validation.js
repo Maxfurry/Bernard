cc662b98d52458c6a1892fb7d855282574538585
unknown <ananioluwatobiloba2000@gmail.com>
2021-01-30 01:47:33 +0100

    modified nd created moddleware

---
+++ middleware/AdmissionController.validation.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+const Joi = require('joi');
+
+const validateParms = {
+  admitPatient: (obj) => {
+    const JoiSchema = Joi.object({
+      patientId: Joi.string().min(34).max(36),
+      admittedOn: Joi.date(),
+      dischargedOn: Joi.date(),
+      roomNumber: Joi.string().min(1),
+      bedNumber: Joi.string().min(1)
+    });
+
+    return JoiSchema.validate({ patientId, admittedOn, dischargedOn, roomNumber, bedNumber } = obj);
+  },
+};
+
+module.exports = validateParms;
