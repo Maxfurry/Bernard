a7d240032b22cf46ac6a50947c42b6462a084378
unknown <ananioluwatobiloba2000@gmail.com>
2021-01-31 21:18:14 +0100

    removed duplicate routes

--- routers/PatientRouter.js
+++ routers/PatientRouter.js
@@ -10,9 +10,6 @@ router.get('/search', PatientController.searchForPatient);
 // to get a patient by their Id
 router.get('/profile/:id', PatientController.getPatientById);
 
-// to register a patient
-router.post('/', PatientController.register);
-
 // get all patients
 router.get('/all', PatientController.getAllPatients);
 
