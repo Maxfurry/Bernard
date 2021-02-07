b1ffefd81cd1aad85245e02ce89d6a31f4275548
unknown <ananioluwatobiloba2000@gmail.com>
2021-02-07 15:54:35 +0100

    created data validation for registering patient controller

--- middleware/PatientController.validation.js
+++ middleware/PatientController.validation.js
@@ -2,6 +2,22 @@
 const Joi = require('joi');
 
 const validateParms = {
+  registerPatient: (obj) => {
+    const JoiSchema = Joi.object({
+      email: Joi.string().email().max(150),
+      password: Joi.string().min(2).max(150),
+      firstName: Joi.string().min(2).max(150),
+      lastName: Joi.string().min(2).max(150),
+      dateOfBirth: Joi.string().min(2).max(150),
+      gender: Joi.string().min(2).max(50),
+      phoneNumber: Joi.string().min(2).max(150),
+      height: Joi.string().min(2).max(10),
+      weight: Joi.string().min(2).max(10),
+      bloodGroup: Joi.string().min(1).max(10),
+      genotype: Joi.string().min(2).max(10)
+    });
+    return JoiSchema.validate({ email, password, firstName, lastName, dateOfBirth, gender, phoneNumber, height, weight, bloodGroup, genotype } = obj);
+  },
   searchForPatient: (obj) => {
     const JoiSchema = Joi.object({
       name: Joi.string().min(2).max(150),
