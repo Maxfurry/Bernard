fd0890e094dcc10c7aad384afa691e28e10fd77d
unknown <ananioluwatobiloba2000@gmail.com>
2021-02-27 00:25:14 +0100

    updated routers by adding admin and auth routers

--- routers/index.js
+++ routers/index.js
@@ -4,10 +4,12 @@ const AuthRouter = require('./AuthRouter');
 const PatientRouter = require('./PatientRouter');
 const AdmitRouter = require('./AdmissionRouter');
 const PatientLabReportRouter = require('./LaboratoryRouter');
+const AdminRouter = require('./AdminRouter');
 
 router.use('/auth', AuthRouter);
 router.use('/patient', PatientRouter);
 router.use('/patient/admit', AdmitRouter);
 router.use('/lab/report', PatientLabReportRouter);
+router.use('/admin', AdminRouter);
 
 module.exports = router;
