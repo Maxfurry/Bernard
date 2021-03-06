12d786eeb8162504edeb519b0998eb67f09a4fa8
unknown <ananioluwatobiloba2000@gmail.com>
2021-03-06 02:46:13 +0100

    created admin seeds

---
+++ database/seeders/20210305214837-admin.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     *
+    */
+
+   return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";').then(async ()=> {
+    const saltRounds = 10;
+    const hashedPassword = bcrypt.hashSync('test1234', saltRounds); // <<<---- TYPE THE PASSWORD HERE
+
+    return await queryInterface.bulkInsert('Employee', [{
+       id: Sequelize.literal('uuid_generate_v4()'),
+       email: 'test@gmail.com',
+       password: hashedPassword, //NOTE: this password should be in .env file
+       role: 'ADMIN',
+       createdAt: new Date(),
+       updatedAt:  new Date()
+     }], {});
+  });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    return queryInterface.bulkDelete('Employee', [{
+      email :'test@gmail.com'
+    }])
+  }
+};
