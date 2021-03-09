5d582fabd89520a7fdb1fd9bb98b957a15f00fde
unknown <ananioluwatobiloba2000@gmail.com>
2021-03-09 12:39:52 +0100

    added new column testName

--- database/migrations/20210208142348-create-laboratory.js
+++ database/migrations/20210208142348-create-laboratory.js
@@ -17,6 +17,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      testName: {
+        type: Sequelize.STRING,
+         allowNull: false
+      },
       comment: Sequelize.STRING,
       createdAt: {
         allowNull: false,
