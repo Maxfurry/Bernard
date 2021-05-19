9cf94003c7b92138a6352985840918e2f76c8526
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2021-05-19 21:07:01 +0100

    added timeline model

---
+++ database/models/timeline.js
@@ -0,0 +1,47 @@
+const {
+    Model
+  } = require('sequelize');
+  
+  module.exports = (sequelize, DataTypes) => {
+    class Timeline extends Model {
+      /**
+       * Helper method for defining associations.
+       * This method is not a part of Sequelize lifecycle.
+       * The `models/index` file will call this method automatically.
+       */
+      static associate(models) {
+        this.belongsTo(models.Patient, {
+          foreignKey: 'patientId',
+          as: 'patient_details'
+        });
+      }
+    }
+    Timeline.init({
+        id: {
+            allowNull: false,
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4
+          },
+          title: {
+            allowNull: false,
+            type: DataTypes.STRING
+          },
+          date: {
+            allowNull: false,
+            type: DataTypes.STRING
+          },
+          description: {
+            type: DataTypes.STRING
+          },
+          createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+          },
+    }, {
+      sequelize,
+      modelName: 'Timelines',
+    });
+    return Timeline;
+  };
+  
